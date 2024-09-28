d6f61cb0bf3d4d453d6333f6877163d7db27e59b
wafiqmuhaz <wafiqmuhaz@gmail.com>
2024-09-28 07:51:59 +0000

    undo

--- lib/Screen/Home_Screen/Widget_Screen/content3.dart
+++ lib/Screen/Home_Screen/Widget_Screen/content3.dart
@@ -181,7 +181,7 @@ class DesktopHeaderContent1 extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(top: 25, left: 550),
                 child: Image.asset(
-                  "assets/image/sosialisasidakwah.jpg",
+                  "assets/image/item4.png",
                   height: 120,
                   fit: BoxFit.cover,
                 ),
@@ -941,7 +941,7 @@ class MobileHeaderContent1 extends StatelessWidget {
                           )
                         ],
                         image: DecorationImage(
-                            image: AssetImage("assets/image/sosialisasidakwah.jpg"),
+                            image: AssetImage("assets/image/develop.jpeg"),
                             fit: BoxFit.cover)),
                   ),
                   Padding(
